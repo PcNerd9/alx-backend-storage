b8682442d5005531797bf43e297a96bf5b6d4110
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-08-25 14:38:04 +0100

    resolve the bug in the 3-glam_rock.sql script v3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- querys metal_bands table to list all bands with Glam rock
 -- as their main style, ranked by their longevity
 
-SELECT band_name, IF(split IS NULL, 2020 - formed, split - formed)
+SELECT band_name, IF(split IS NULL, 2022 - formed, split - formed)
 AS lifespan FROM metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;

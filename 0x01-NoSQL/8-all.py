058a430829e97daf2f1123278337968b6291360d
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-08-28 05:23:38 +0100

    resolve requirement issues v2

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-
+"""
+lists all documents in a collections
+"""
 import pymongo
 
 
 def list_all(mongo_collection):
+    """
+    lists all documents in a collections
+    """
     result = mongo_collection.find()
     if not result:
         return []
     return list(result)
-        

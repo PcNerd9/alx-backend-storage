4e0e34604f8a9c3820ac58b7caec194f1a1bc9d7
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-08-30 13:42:04 +0100

    resolve the bug in the 12-log_stats.py file v2

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,24 +7,27 @@ logs stored in Mongodb
 
 from pymongo import MongoClient
 
+def main():
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client.logs
+    collection = db.nginx
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client.logs
-collection = db.nginx
+    total_count = collection.count_documents({})
+    get_count = collection.count_documents({"method": "GET"})
+    post_count = collection.count_documents({"method": "POST"})
+    put_count = collection.count_documents({"method": "PUT"})
+    patch_count = collection.count_documents({"method": "PATCH"})
+    delete_count = collection.count_documents({"method": "DELETE"})
+    status = collection.count_documents({"path": "/status"})
 
-total_count = collection.count_documents({})
-get_count = collection.count_documents({"method": "GET"})
-post_count = collection.count_documents({"method": "POST"})
-put_count = collection.count_documents({"method": "PUT"})
-patch_count = collection.count_documents({"method": "PATCH"})
-delete_count = collection.count_documents({"method": "DELETE"})
-status = collection.count_documents({"path": "/status"})
+    print("{} logs".format(total_count))
+    print("Methods:")
+    print("\tmethod GET: {}".format(get_count))
+    print("\tmethod POST: {}".format(post_count))
+    print("\tmethod PUT: {}".format(put_count))
+    print("\tmethod PATCH: {}".format(patch_count))
+    print("\tmethod DELETE: {}".format(delete_count))
+    print("{} status check".format(status))
 
-print("{} logs".format(total_count))
-print("Methods:")
-print("\tmethod GET: {}".format(get_count))
-print("\tmethod POST: {}".format(post_count))
-print("\tmethod PUT: {}".format(put_count))
-print("\tmethod PATCH: {}".format(patch_count))
-print("\tmethod DELETE: {}".format(delete_count))
-print("{} status check".format(status))
+if __name__ == "__main__":
+    main()

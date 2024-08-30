f0d97d2b3198eddb2e522029472f57a9b001839b
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-08-30 13:37:49 +0100

    wrote a python script that provides some stats about Nginx logs stored in MongoDB

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+contains a function that provides stats about Nginx
+logs stored in Mongodb
+"""
+
+from pymongo import MongoClient
+
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client.logs
+collection = db.nginx
+
+total_count = collection.count_documents({})
+get_count = collection.count_documents({"method": "GET"})
+post_count = collection.count_documents({"method": "POST"})
+put_count = collection.count_documents({"method": "PUT"})
+patch_count = collection.count_documents({"method": "PATCH"})
+delete_count = collection.count_documents({"method": "DELETE"})
+status = collection.count_documents({"path": "/status"})
+
+print("{} logs".format(total_count))
+print("Methods:")
+print("\tmethod GET: {}".format(get_count))
+print("\tmethod POST: {}".format(post_count))
+print("\tmethod PUT: {}".format(put_count))
+print("\tmethod PATCH: {}".format(patch_count))
+print("\tmethod DELETE: {}".format(delete_count))
+print("{} status check".format(status))
